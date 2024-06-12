3ed880d3fbaaa943c7ca902623b163df009db65b
Shay Mayer <shaymayer2001@gmail.com>
2024-06-13 00:59:26 +0300

    Update Main.java

--- Snake/src/ShayMayer/Main.java
+++ Snake/src/ShayMayer/Main.java
@@ -5,8 +5,8 @@ import ShayMayer.GameManagment.SnakeGame;
 public class Main {
 
     public static void main(String[] args) {
-	    SnakeGame snake = new SnakeGame();
-	    while(true)
-	        snake.update();
+        SnakeGame snake = new SnakeGame();
+        while(true)
+            snake.update();
     }
 }
